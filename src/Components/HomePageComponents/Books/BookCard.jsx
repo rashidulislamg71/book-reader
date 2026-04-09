3a0a1d4087c636e4ad35f0ac39f21755674df1cf
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-04-09 07:53:12 +0600

    fetch data and show book card

--- src/Components/HomePageComponents/Books/BookCard.jsx
+++ src/Components/HomePageComponents/Books/BookCard.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { use } from 'react'
 import img from "../../../assets/quran.png"
 import { FiStar } from 'react-icons/fi'
 
-function BookCard() {
+function BookCard({ book }) {
+
+
+    const { bookName, author, image, rating, category, tags } = book
+
+    console.log(bookName)
+
     return (
         <div className=' '>
             <div className='group cursor-pointer hover:shadow-xl duration-300 transition-all w-80 h-100 p-4 border border-gray-200 rounded-2xl flex flex-col gap-3  justify-between'>
                 <div className='bg-gray-100 h-[60%] w-full overflow-hidden rounded-2xl flex justify-center items-center'>
-                    <div className='w-[50%] '>
-                        <img className='rounded-2xl transition duration-300 group-hover:scale-105' src={img} alt="" />
+                    <div className='w-[50%] flex justify-center'>
+                        <img className='rounded transition  h-44 duration-300 group-hover:scale-105' src={image} alt="" />
                     </div>
+                </div>
+                <div className='flex items-center gap-3'>
+                    {
+                        tags.map((data, inx) => (
+                            <li className='list-none' key={inx}>
+                                <div className="badge badge-soft badge-success text-sm ">{data}</div>
+                            </li>
+                        ))
+                    }
                 </div>
                 <div>
                     <div>
-                        <h3 className='text-gray-700 font-bold text-2xl'>The Name of Book</h3>
-                        <p className='text-gray-700' >By: Name of Writer</p>
+                        <h3 className='text-gray-700 font-bold text-[18px] mb-2 '>{bookName}</h3>
+                        <p className='text-gray-700' >By: {author} </p>
                     </div>
                 </div>
                 <div className='pt-2 w-full border-t border-gray-300 flex justify-between items-center' >
-                    <p className='text-gray-400'>Which type is book</p>
+
+                    <p>{category} </p>
+
                     <div className='flex justify-between items-center gap-1'>
-                        <p className='text-gray-500'>5.00</p>
+                        <p className='text-gray-500'>{rating}</p>
                         <FiStar className='text-orange-300' />
                     </div>
                 </div>
--- src/Components/HomePageComponents/Books/Books.jsx
+++ src/Components/HomePageComponents/Books/Books.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { Suspense, use } from 'react'
 import BookCard from './BookCard'
 
+const fetchBookData = fetch("/book.json").then(res => res.json());
+
+
+
 function Books() {
+      const books = use(fetchBookData)
+      console.log(books)
   return (
     <div className='px-4 md:px-10 py-10 md:py-15'>
       <h1 className='text-center text-3xl md:text-4xl font-bold mb-10 md:mb-15'>Books</h1>
- 
+
       <div className='grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10'>
-           <BookCard />
-         <BookCard />
-         <BookCard />
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+         {
+          books.map(book => <BookCard key={book.id} book={book} />)
+         }
+        </Suspense>
       </div>
 
     </div>
