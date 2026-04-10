f63fdeaec61e7ed9060a479f5bfdf23f9d07d513
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-04-10 07:02:08 +0600

    add toggle btn for show read list book and wish list book

--- src/Pages/ListedBooksPage.jsx
+++ src/Pages/ListedBooksPage.jsx
@@ -2,13 +2,20 @@ import React, { useContext, useState } from 'react'
 import { DataContext } from '../Context/Context'
 
 function ListedBooksPage() {
-  const {readedBooks} = useContext(DataContext);
+  const [toggleBtn, setToggleBtn] = useState("readList")
 
+  const { readedBooks, wishList } = useContext(DataContext);
 
-  
+  const btnStyle = `bg-none px-3 rounded-2xl text-black pb-1 `
+  const btnActiveStyle = `bg-blue-600 px-3 rounded-2xl text-white pb-1 `
 
   return (
-    <div>ListedBooksPage</div>
+    <div>
+      <div className=' rounded-2xl border w-fit m-auto'>
+        <button onClick={() => setToggleBtn("readList")} className={`${toggleBtn === "readList" ? btnActiveStyle : btnStyle} cursor-pointer`}>Success</button>
+        <button onClick={() => setToggleBtn("wishList")} className={`${toggleBtn === "wishList" ? btnActiveStyle : btnStyle} cursor-pointer`} >Success</button>
+      </div>
+    </div>
   )
 }
 
